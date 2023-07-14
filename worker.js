219a90b42238be2d30d22f662d052a8875ac391b
Aryeh Mischel <aryehmischel@gmail.com>
2023-07-13 21:35:40 -0400

    updated worker

--- worker.js
+++ worker.js
@@ -1,3 +1,4 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
 var canvas
 
 onmessage = async (evt) => {
@@ -5,23 +6,51 @@ onmessage = async (evt) => {
 
     switch(evt.data.work) {
         case "init":
-          // code block
-          console.log("initializing...")
+          console.log("initialized offline canvas")
+          canvas = evt.data.canvas
           break;
-        case y:
-          // code block
+        case "resize":
+          canvas.height =  evt.data.canvasHeight
+          canvas.width =  evt.data.canvasHeight
           break;
-        default:
-          // code block
+        case "crop-resize-stereo":
+            let imageURLS = evt.data.imageURLS;
+            for(let i = 0; i < imageURLS.length; i++){
+              drawFitToCanvas(canvas, imageURLS[i])
+             await delay(1000);
+          
       }
 
-    if(evt.data.work == "init"){
-        console.log("initialized offline canvas")
-        canvas = evt.data.canvas
-        return
-    }
 
+      console.log("done")
+    // self.postMessage("right back at you cheif")
+
+}}
+
+async function drawFitToCanvas(canvas, imageUrl){
+  
     
-    self.postMessage("right back at you cheif")
+    
+    let ctx = canvas.getContext("2d");
+  
+    const imgblob = await fetch(imageUrl)
+     .then(r => r.blob());
+    const img = await createImageBitmap(imgblob)
+    
+  
+    let scaleFactor = Math.max(canvas.width / img.width, canvas.height / img.height);
+  
+    let newWidth = img.width * scaleFactor;
+    let newHeight = img.height * scaleFactor;
+    
+    let x = (canvas.width / 2) - (newWidth / 2);
+    let y = (canvas.height / 2) - (newHeight / 2);
+    
 
+    ctx.drawImage(img, x, y, newWidth, newHeight * 2); // newHeight * 2
+     
+    await delay(1000);
+    self.postMessage("done")
+  
+  
 }
