088120c9cfe82a46904b08306e046ac9ab977cad
Aryeh Mischel <aryehmischel@gmail.com>
2023-07-13 21:16:12 -0400

    update worker

--- worker.js
+++ worker.js
@@ -1,6 +1,27 @@
+var canvas
+
 onmessage = async (evt) => {
 
-    console.log("recieved message from worker")
+
+    switch(evt.data.work) {
+        case "init":
+          // code block
+          console.log("initializing...")
+          break;
+        case y:
+          // code block
+          break;
+        default:
+          // code block
+      }
+
+    if(evt.data.work == "init"){
+        console.log("initialized offline canvas")
+        canvas = evt.data.canvas
+        return
+    }
+
+    
     self.postMessage("right back at you cheif")
 
 }
