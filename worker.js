95b8c5ee1337bda46c5a2ca6ca585cfd6e5012bd
Aryeh Mischel <aryehmischel@gmail.com>
2023-07-17 08:21:48 -0400

    update index

--- worker.js
+++ worker.js
@@ -1,53 +1,260 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 var canvas
+// let firstMessage = false
+
+const WorkMode = {
+  "init": "init",
+  "mono": 0,
+  "stereo": 1
+  // 0: "mono",
+  // 1: "stereo",
+  // 2: 2,
+  // 3: 3
+}
+
 
 onmessage = async (evt) => {
 
-  console.log("worker is messaged")
-
-
-  switch (evt.data.work) {
-    case "init":
-      console.log("initialized offline canvas")
-      canvas = evt.data.canvas
-      break;
-    case "resize-canvas":
-      canvas.height = evt.data.canvasHeight / 2
-      canvas.width = evt.data.canvasHeight
-      break;
-    case "stereo":
-      console.log("crop-resize-stereo")
-      let imageURLS = evt.data.imageURLS;
-      CropResizeStereo(canvas, imageURLS)
-      break;
-
-    case "mono":
-      console.log("mono")
-      resize2(canvas, evt.data.imageURLS)
-      // resize(canvas, evt.data.imageURLS)
-      break;
-
-    case "crop-resize-stereo-L|R":
-      console.log("crop-resize-stereo")
-      break;
-
-    case "crop-resize-stereo-R|L":
-      console.log("crop-resize-stereo")
-      break;
-
-
-    case "crop-resize-stereo-R/L":
-      console.log("crop-resize-stereo")
-      break;
-
-    case "fuck":
-      console.log("fuck")
-      break;
-
-      console.log("done")
-    // self.postMessage("right back at you cheif")
+  if(evt.data.inputMode == "init"){
+    canvas = evt.data.canvas;
 
   }
+
+  else if(canvas == undefined){
+    console.log("no canvas selected. select one.")
+    return;
+  }
+
+  console.log("worker is messaged",evt.data.inputMode )
+  // console.log(WorkMode["mono"])
+    const imageURLS = evt.data.imageURLS;
+    const imageSizes = evt.data.imageSizes;
+    const inputMode = evt.data.inputMode;
+
+
+  switch (WorkMode[evt.data.inputMode]) {
+
+    case 0:
+      console.log("mono")
+      mono()
+      break;
+    case 1:
+      console.log("stereo")
+      stereo()
+      break;
+
+
+
+
+  }
+
+  
+    // skies =  [s, s, s, s, s, ]
+
+   // s = [[a], [ab], etc ] 
+
+    // operating meshes  [a, b, a, b, a, b]
+
+    //one sky per image
+
+    //for image in images
+    //    
+
+
+  async function mono(){
+
+    
+    // determineLineSegments(); //this code will be done in the main thread
+    
+    let Segments = 4
+
+
+    nextStep(canvas, imageURLS, imageSizes, inputMode)
+
+
+  }
+
+  async function stereo(){
+
+
+
+
+
+
+
+    nextStep(canvas, imageURLS, imageSizes, inputMode)
+
+
+    
+
+
+
+    
+
+ 
+
+
+
+  }
+
+
+  async function nextStep(canvas, imageURLS, imageSizes, inputMode){
+
+    let ctx = canvas.getContext("2d");
+
+    for (let i = 0; i < imageURLS.length; i++) {
+      const imgblob = await fetch(imageURLS[i])
+        .then(r => r.blob());
+      const img = await createImageBitmap(imgblob)
+  
+  
+      // let scaleFactor = Math.max(canvas.width / img.width, canvas.height / img.height);
+      // console.log(scaleFactor)
+  
+      // let newWidth = img.width * scaleFactor;
+      // let newHeight = img.height * scaleFactor;
+  
+      // let x = (canvas.width / 2) - (newWidth / 2);
+      // let y = (canvas.height / 2) - (newHeight / 2);
+  
+      // // console.log(x, y)
+  
+      // // ctx.drawImage(img, x, y, newWidth, newHeight);
+      // ctx.drawImage(img, x, y, canvas.width / 4, canvas.height);
+  
+      // // ctx.drawImage(img, 0, 0);
+  
+      // // await delay(4000);
+      // // self.postMessage("done mono")
+      // // postMessage({blob: canvas.convertToBlob()})
+  
+      canvas.convertToBlob().then((blob) => {
+        postMessage({ blob })
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+      })
+
+     
+      if(inputMode == "stereo"){
+
+        // ctx.drawImage(img, x, y, canvas.width / 3, canvas.height);
+
+
+        canvas.convertToBlob().then((blob) => {
+          postMessage({ blob })
+          ctx.clearRect(0, 0, canvas.width, canvas.height)
+        })
+      
+      }
+  
+    }
+  }
+  
+
+
+
+
+
+
+
+async function resize2(canvas, imageURLS, imageSizes) {
+  let ctx = canvas.getContext("2d");
+
+  for (let i = 0; i < imageURLS.length; i++) {
+    const imgblob = await fetch(imageURLS[i])
+      .then(r => r.blob());
+    const img = await createImageBitmap(imgblob)
+
+
+    let scaleFactor = Math.max(canvas.width / img.width, canvas.height / img.height);
+    console.log(scaleFactor)
+
+    let newWidth = img.width * scaleFactor;
+    let newHeight = img.height * scaleFactor;
+
+    let x = (canvas.width / 2) - (newWidth / 2);
+    let y = (canvas.height / 2) - (newHeight / 2);
+
+    // console.log(x, y)
+
+    // ctx.drawImage(img, x, y, newWidth, newHeight);
+    ctx.drawImage(img, x, y, canvas.width / 4, canvas.height);
+
+    // ctx.drawImage(img, 0, 0);
+
+    // await delay(4000);
+    // self.postMessage("done mono")
+    // postMessage({blob: canvas.convertToBlob()})
+
+    canvas.convertToBlob().then((blob) => {
+      postMessage({ blob })
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+      // optional: add the fillrect to clear the canvas.
+    })
+
+    // await delay(1000);
+
+
+  }
+
+
+
+};
+
+
+
+
+
+
+
+  // all being sliced into segments
+  // all being
+
+
+  // switch (evt.data.work) {
+  //   case "init":
+  //     console.log("initialized offline canvas")
+  //     canvas = evt.data.canvas
+  //     break;
+  //   case "resize-canvas":
+  //     canvas.height = evt.data.canvasHeight / 2
+  //     canvas.width = evt.data.canvasHeight
+  //     break;
+  //   case "stereo":
+  //     console.log("crop-resize-stereo")
+  //     let imageURLS = evt.data.imageURLS;
+  //     CropResizeStereo(canvas, imageURLS)
+  //     break;
+
+  //   case "mono":
+  //     console.log("mono", evt.data.imageSizes)
+  //     resize2(canvas, evt.data.imageURLS, evt.data.imageSizes)
+  //     // resize(canvas, evt.data.imageURLS)
+  //     break;
+
+  //   case "crop-resize-stereo-L|R":
+  //     console.log("crop-resize-stereo")
+  //     break;
+
+  // case "crop-resize-stereo-R|L":
+  //   console.log("crop-resize-stereo")
+  //   break;
+
+
+  // case "crop-resize-stereo-R/L":
+  //   console.log("crop-resize-stereo")
+  //   break;
+
+  // case "fuck":
+  //   console.log("fuck")
+  //   break;
+
+  //   console.log("done")
+  // self.postMessage("right back at you cheif")
+
+  // }
 }
 
 
@@ -153,13 +360,6 @@ async function drawFitToCanvas(canvas, imageUrl) {
   let newWidth = img.width * scaleFactor;
   let newHeight = img.height * scaleFactor;
 
-  // let x = (canvas.width / 2) - (newWidth / 2);
-  // let y = (canvas.height / 2) - (newHeight / 2);    
-
-  // ctx.drawImage(img, x , y, newWidth, newHeight * 2); 
-  // ctx.drawImage(img, x , y, newWidth, newHeight * 2 ); 
-
-  // console.log("before message")
   ctx.drawImage(img, 0, 0, img.width, img.height / 2, 0, 0, newWidth, newHeight);
   await delay(4000);
 
@@ -208,48 +408,3 @@ async function drawFitToCanvas(canvas, imageUrl) {
 
 // canvas.width = img.width
 // canvas.height = img.height
-
-async function resize2(canvas, imageURLS) {
-  let ctx = canvas.getContext("2d");
-
-  for (let i = 0; i < imageURLS.length; i++) {
-    const imgblob = await fetch(imageURLS[i])
-      .then(r => r.blob());
-    const img = await createImageBitmap(imgblob)
-
-
-
-    let scaleFactor = Math.max(canvas.width / img.width, canvas.height / img.height);
-    console.log(scaleFactor)
-
-    let newWidth = img.width * scaleFactor;
-    let newHeight = img.height * scaleFactor;
-
-    let x = (canvas.width / 2) - (newWidth / 2);
-    let y = (canvas.height / 2) - (newHeight / 2);
-
-    // ctx.drawImage(img, x, y, newWidth, newHeight);
-    ctx.drawImage(img, x, y, canvas.width / 4, canvas.height);
-
-    // ctx.drawImage(img, 0, 0);
-
-    // await delay(4000);
-    // self.postMessage("done mono")
-    // postMessage({blob: canvas.convertToBlob()})
-
-    canvas.convertToBlob().then((blob) => {
-      postMessage({ blob })
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-
-      // optional: add the fillrect to clear the canvas.
-    })
-
-    // await delay(1000);
-
-
-  }
-
-
-
-};
