4b2afa8156cbda0e5a5020ce2b3cbbcea0c99013
Aryeh Mischel <aryehmischel@gmail.com>
2023-07-19 20:28:36 -0400

    updated stereoscopic

--- worker.js
+++ worker.js
@@ -18,21 +18,21 @@ const WorkMode = {
 
 onmessage = async (evt) => {
 
-  if(evt.data.inputMode == "init"){
+  if (evt.data.inputMode == "init") {
     canvas = evt.data.canvas;
 
   }
 
-  else if(canvas == undefined){
+  else if (canvas == undefined) {
     console.log("no canvas selected. select one.")
     return;
   }
 
-  console.log("worker is messaged",evt.data.inputMode )
+  console.log("worker is messaged", evt.data.inputMode)
   // console.log(WorkMode["mono"])
-    // const imageURLS = evt.data.imageURLS;
-    // const imageSizes = evt.data.imageSizes;
-    const inputMode = evt.data.inputMode;
+  // const imageURLS = evt.data.imageURLS;
+  // const imageSizes = evt.data.imageSizes;
+  const inputMode = evt.data.inputMode;
 
 
   switch (WorkMode[evt.data.inputMode]) {
@@ -43,7 +43,7 @@ onmessage = async (evt) => {
       break;
     case 1:
       console.log("stereo")
-      stereo()
+      stereo(evt.data.imageURLS)
       break;
     case 2:
       console.log("resizing canvas...")
@@ -57,24 +57,24 @@ onmessage = async (evt) => {
 
   }
 
-  
-    // skies =  [s, s, s, s, s, ]
 
-   // s = [[a], [ab], etc ] 
+  // skies =  [s, s, s, s, s, ]
 
-    // operating meshes  [a, b, a, b, a, b]
+  // s = [[a], [ab], etc ] 
 
-    //one sky per image
+  // operating meshes  [a, b, a, b, a, b]
 
-    //for image in images
-    //    
+  //one sky per image
+
+  //for image in images
+  //    
 
 
-  async function mono(imageURLS){
+  async function mono(imageURLS) {
 
-    
+
     // determineLineSegments(); //this code will be done in the main thread
-    
+
     let Segments = 8
 
 
@@ -88,7 +88,15 @@ onmessage = async (evt) => {
 
 
 
-  async function stereo(){
+  async function stereo(imageURLS) {
+
+
+
+
+
+    sliceResizeImageStereo(imageURLS)
+
+    // nextStep(canvas, imageURLS, imageSizes, inputMode)
 
 
 
@@ -96,23 +104,15 @@ onmessage = async (evt) => {
 
 
 
-    nextStep(canvas, imageURLS, imageSizes, inputMode)
 
 
-    
-
-
-
-    
-
- 
 
 
 
   }
 
 
-  async function sliceImages(imageURLS){
+  async function sliceImages(imageURLS) {
     let ctx = canvas.getContext("2d");
     const lineSegments = 4;
 
@@ -121,16 +121,16 @@ onmessage = async (evt) => {
         .then(r => r.blob());
       const img = await createImageBitmap(imgblob)
 
-      canvas.height = img.height 
+      canvas.height = img.height
       canvas.width = img.width / lineSegments;
 
-      for(let i = 1; i <= lineSegments; i++){
-        ctx.drawImage(img, img.width - canvas.width * i, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height )
+      for (let i = 1; i <= lineSegments; i++) {
+        ctx.drawImage(img, img.width - canvas.width * i, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height)
 
         canvas.convertToBlob().then((blob) => {
           postMessage({ blob })
           ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
         })
 
 
@@ -142,16 +142,16 @@ onmessage = async (evt) => {
   }
 
 
-  
-  
-  
-  
-  
-  
-  async function resizeSliceImages(imageURLS){
+
+
+
+
+
+
+  async function resizeSliceImages(imageURLS) {
     let ctx = canvas.getContext("2d");
     const lineSegments = 4;
-    canvas.width =  ScaleWidth / lineSegments
+    canvas.width = ScaleWidth / lineSegments
 
     for (let i = 0; i < imageURLS.length; i++) {
       const imgblob = await fetch(imageURLS[i])
@@ -161,34 +161,147 @@ onmessage = async (evt) => {
 
       let scaleFactor = Math.max(canvas.width / img.width, canvas.height / img.height);
       console.log(scaleFactor)
-  
+
       let newWidth = img.width * scaleFactor;
       let newHeight = img.height * scaleFactor;
-  
+
       let x = (canvas.width / 2) - (newWidth / 2);
       let y = (canvas.height / 2) - (newHeight / 2);
 
-   
-      for(let i = 1; i <= lineSegments; i++){
+
+      for (let i = 1; i <= lineSegments; i++) {
         ctx.drawImage(img,
-           imgWidth -(i *  imgWidth/lineSegments) , 0,
-           (img.width / lineSegments), img.height,
-            0, 0,
-            newWidth / lineSegments, newHeight)
+          imgWidth - (i * imgWidth / lineSegments), 0,
+          (img.width / lineSegments), img.height,
+          0, 0,
+          newWidth / lineSegments, newHeight)
 
         canvas.convertToBlob().then((blob) => {
           postMessage({ blob })
           ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
         })
-        
+
 
       }
 
 
     }
 
-  }}
-  
-  
+  }
+}
+
+
+
+//slicing 360 L/R photos for spherical projection. Everything should be renamed when refactoring...
+//top image is left eye
+//bottom image is right eye
+async function sliceResizeImageStereo(imageURLS) {
+
+  let ctx = canvas.getContext("2d");
+  const lineSegments = 4;
+  canvas.width = ScaleWidth / lineSegments
+
+  for (let i = 0; i < imageURLS.length; i++) {
+    const imgblob = await fetch(imageURLS[i])
+      .then(r => r.blob());
+    const img = await createImageBitmap(imgblob)
+    let imgWidth = img.width;
+
+    let scaleFactor = Math.max(canvas.width / img.width, canvas.height / img.height);
+    // let scaleFactor = Math.max(canvas.width / img.width, canvas.height / (img.height / 2));
+    
+    console.log(scaleFactor)
+
+    let newWidth = img.width * scaleFactor;
+    let newHeight = img.height * scaleFactor;
+
+    // ctx.drawImage(img, 
+    //   canvas.width *2, canvas.height /2,
+    //   canvas.width, canvas.height /2,
+    //   0, 0,
+    //   canvas.width, canvas.height /2 )
+
+    //draw left
+    for (let i = 1; i <= lineSegments; i++) {
+      ctx.drawImage(img,
+        imgWidth - (i * imgWidth / lineSegments), 0,
+        (img.width / lineSegments), img.height/2,
+        0, 0,
+        canvas.width, canvas.height)
+
+      canvas.convertToBlob().then((blob) => {
+        postMessage({ blob })
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      })
+
+
+    }
+
+
+    for (let i = 1; i <= lineSegments; i++) {
+      ctx.drawImage(img,
+        imgWidth - (i * imgWidth / lineSegments), img.height / 2,
+        (img.width / lineSegments), img.height/2,
+        0, 0,
+        canvas.width, canvas.height)
+
+      canvas.convertToBlob().then((blob) => {
+        postMessage({ blob })
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      })
+
+
+    }
+
+
+    // for (let i = 1; i <= lineSegments; i++) {
+    //   ctx.drawImage(img,
+    //     imgWidth - (i * imgWidth / lineSegments), 0,
+    //     (img.width / lineSegments), img.height/2,
+    //     0, 0,
+    //     newWidth, newHeight)
+
+    //   canvas.convertToBlob().then((blob) => {
+    //     postMessage({ blob })
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //   })
+
+
+    // }
+
+
+    //draw right
+    // for (let i = 1; i <= lineSegments; i++) {
+    //   ctx.drawImage(img,
+    //     imgWidth - (i * imgWidth / lineSegments), 0,
+    //     (img.width / lineSegments), img.height,
+    //     0, 0,
+    //     newWidth / lineSegments, newHeight)
+
+    //   canvas.convertToBlob().then((blob) => {
+    //     postMessage({ blob })
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //   })
+
+
+    // }
+
+
+
+
+
+
+  }
+
+}
+
+
+
+
+
 
